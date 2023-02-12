b58e76db12d588ed0efbb0371769e78d1c462ed4
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-12 20:53:41 +0200

    Add Unsplash API

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-const Home = () => {
+import { UNSPLASH_BASE_URL, UNSPLASH_ACCESS_KEY } from "@/constants/unsplash";
+
+const Home = async () => {
+  // TODO: Unsplash JS API, it has TypeScript definitions
+  const photos = await fetch(`${UNSPLASH_BASE_URL}/photos`, {
+    headers: { Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}` },
+  }).then((res) => res.json());
+
   return (
     <main>
-      <h1>Hello world! 👋🏻</h1>
+      <h1 className="text-5xl font-bold">Hello world! 👋🏻</h1>
+      {photos.map((photo) => (
+        <img
+          key={photo.id}
+          src={photo.urls.small}
+          alt={photo.description || photo.alt_description}
+        />
+      ))}
     </main>
   );
 };
