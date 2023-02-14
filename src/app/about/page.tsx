a5755b2113307b583cd37692801307879c285d98
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-14 20:51:30 +0200

    Improve about page CSS

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <h1>About</h1>
-      ABOUT PAGE!!!!!!!!!!!11
+      <h1 className="mb-8 mt-4 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+        About
+      </h1>
+      <p>ABOUT PAGE!!!!!!!!!!!11</p>
     </>
   );
 };
