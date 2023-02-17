672f1062899302a816ed7330ae2da082202ab090
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-17 15:40:16 +0200

    Improve error page CSS, add error stack

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import { H1 } from 'components/headings';
+import { H1, H2 } from 'components/headings';
 
 const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
     <div>
       <H1>Oh no!</H1>
-      <p>{`Hmm... We couldn't load this: ${error}`}</p>
-      <button onClick={reset}>Try again</button>
+      <p>{`Hmm... We couldn't load this: ${error.message}`}</p>
+      <button className='text-blue-700 underline' onClick={reset}>
+        Try again
+      </button>
+      <H2>Error details</H2>
+      <pre>{error.stack}</pre>
     </div>
   );
 };
