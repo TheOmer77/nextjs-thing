9114cc797ad4f6411da1b166a0e6675a2bbf1eba
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-17 15:19:25 +0200

    Add H1 to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import H1 from 'components/H1';
+
 const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
     <div>
+      <H1>Oh no!</H1>
       <p>{`Hmm... We couldn't load this: ${error}`}</p>
       <button onClick={reset}>Try again</button>
     </div>
