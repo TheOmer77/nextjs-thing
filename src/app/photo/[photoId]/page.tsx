33ba97053c4e164d167529af6261a4b6d4975718
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-17 14:40:28 +0200

    Add actually useful details to photo page

--- src/app/photo/[photoId]/page.tsx
+++ src/app/photo/[photoId]/page.tsx
@@ -11,17 +11,21 @@ const PhotoPage = async ({
   const photo = await getPhotoById(photoId);
   return (
     <>
-      <div>
-        <Image
-          src={photo.urls.full}
-          alt={photo.description || photo.alt_description || photo.id}
-          width={photo.width}
-          height={photo.height}
-          className='absolute left-0 right-0 top-0 h-96 w-full bg-slate-50 object-contain pt-16 sm:h-[32rem] md:h-[36rem] lg:h-[40rem]'
-        />
-      </div>
-      <div className='mt-80 lg:mt-[36rem]'>
-        <p>Photo ID: {photoId}</p>
+      <Image
+        src={photo.urls.full}
+        alt={photo.description || photo.alt_description || photo.id}
+        width={photo.width}
+        height={photo.height}
+        className='absolute left-0 right-0 top-0 h-96 w-full bg-slate-50 object-contain pt-16 sm:h-[32rem] md:h-[36rem] lg:h-[40rem]'
+      />
+      <div className='mt-80 text-slate-900 lg:mt-[36rem]'>
+        <p className='text-xl font-bold'>
+          {photo.description || photo.alt_description || 'Untitled photo'}
+        </p>
+        <p className='text-slate-900/60'>
+          Uploaded by {photo.user.name} on{' '}
+          {new Date(photo.created_at).toLocaleString('en-US')}
+        </p>
       </div>
     </>
   );
